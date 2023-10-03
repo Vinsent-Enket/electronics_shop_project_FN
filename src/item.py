fea2422ae2dc5a5175a145070fa15f6c67e16116
filippov nikita <filippovnr73@gmail.com>
2023-10-03 11:51:55 +0300

    howo 2 complited

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
@@ -31,3 +34,32 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @property
+    def name(self):
+        """
+        возвращает наименование товара
+        """
+        return f'{self.__name}'
+
+    @name.setter
+    def name(self, name):
+        """Метод срабатывает при операции присваивания, устанавливая наименование товара"""
+        if len(name) >= 10:
+            name = name[:10]
+        self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, path_file):
+        cls.all = []
+        # немного помучался с кодировкой, так и должно быть)))?
+        # на стороннем сайте вбил текст из файла и он подсказал мне кодировку,
+        # на маке он пытался воспринять его как utf-8
+        with open(path_file, 'r', encoding='WINDOWS-1251') as csvfile:
+            file = csv.DictReader(csvfile)
+            for line in file:
+                Item(line['name'], line['price'], line['quantity'])
+
+    @staticmethod
+    def string_to_number(string):
+        return int(float(string))
