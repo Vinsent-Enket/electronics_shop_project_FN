8188eafc9f1a3dd9e0c671df8bb353e4a1b0b529
filippov nikita <filippovnr73@gmail.com>
2023-10-10 18:27:49 +0300

    howo 4 complited

--- src/learn.py
+++ src/learn.py
@@ -1,12 +1,1 @@
-# import csv
-# #немного помучался с кодировкой, так и должно быть)))? на стороннем сайте вбил текст из файла и он подсказал мне кодировку, на маке
-# #он пытался воспринять его как utf-8
-# with open('items.csv', 'r',  encoding='WINDOWS-1251') as csvfile:
-#     diict = csv.DictReader(csvfile)
-#     for line in diict:
-#         print(line)
-#
-q = "5.5"
-q = float(q)
-q = int(q)
-print(q)
+raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
