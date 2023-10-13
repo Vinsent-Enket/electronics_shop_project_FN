c930d913345ab116e64971cc8b20f1291fe6e02c
filippov nikita <filippovnr73@gmail.com>
2023-10-13 07:36:41 +0300

    howo 4 complited add comment

--- src/phone.py
+++ src/phone.py
@@ -2,6 +2,13 @@ from src.item import Item
 
 
 class Phone(Item):
+
+    """
+    Откровенно говоря не совсем понял принцип super() ибо не приходится
+    все равно явно объявлять __name, в то время как прайс и количество явно объяляются
+    так же как в Item, но код работает и это наверное главное
+    """
+
     def __init__(self, name: str, price: float, quantity: int, number_of_sim) -> None:
         super().__init__(name, price, quantity)
         self.__name = name
