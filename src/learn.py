a8681cc72fbe28a8bb110d5ebadd21ba301b929f
filippov nikita <filippovnr73@gmail.com>
2023-10-21 11:59:58 +0300

    howo 6 complited

--- src/learn.py
+++ src/learn.py
@@ -1,19 +1,85 @@
-from src.keyboard import Keyboard
+import csv
+
+from src.my_exeption import InstantiateCSVError
 
 
-kb = Keyboard('Dark Project KD87A', 9600, 5)
+class Item:
+    """
+    Класс для представления товара в магазине.
+    """
+    pay_rate = 1.0
+    all = []
 
-assert str(kb) == "Dark Project KD87A"
+    def __init__(self, name: str, price: float, quantity: int) -> None:
+        super().__init__()
+        """
+        Создание экземпляра класса item.
 
-assert str(kb.language) == "EN"
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        """
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
 
-kb.change_lang()
-assert str(kb.language) == "RU"
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
 
-# Сделали EN -> RU -> EN
-kb.change_lang()
-print(str(kb.language))
-assert str(kb.language) == "EN"
+    def __str__(self):
+        return self.__name
 
-kb.language = 'CH'
-# AttributeError: property 'language' of 'Keyboard' object has no setter
+    def __add__(self, other):
+        return self.quantity + other.quantity
+
+    def calculate_total_price(self) -> float:
+        """
+        Рассчитывает общую стоимость конкретного товара в магазине.
+
+        :return: Общая стоимость товара.
+        """
+        return self.quantity * self.price
+
+    def apply_discount(self) -> None:
+        """
+        Применяет установленную скидку для конкретного товара.
+        """
+        self.price = self.price * self.pay_rate
+
+    @property
+    def name(self):
+        """
+        возвращает наименование товара
+        """
+        return f'{self.__name}'
+
+    @name.setter
+    def name(self, name):
+        """Метод срабатывает при операции присваивания, устанавливая наименование товара"""
+        if len(name) >= 10:
+            name = name[:10]
+        self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, path_file):
+        cls.all = []
+        try:
+            with open(path_file, 'r', encoding='WINDOWS-1251') as csvfile:
+                file = csv.DictReader(csvfile)
+                for line in file:
+                    if len(line.keys()) != 3:
+                        raise InstantiateCSVError()
+                    Item(line['name'], line['price'], line['quantity'])
+        except FileNotFoundError:
+            print('Файл не найден')
+
+
+
+    @staticmethod
+    def string_to_number(string):
+        return int(float(string))
+
+Item.instantiate_from_csv('../src/items2.csv')  # создание объектов из данных файла
+
+Item.instantiate_from_csv('../src/items2.csv')  # создание объектов из данных файла
